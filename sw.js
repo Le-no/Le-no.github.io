e2e9e3d26ae51800b67b29e938dc36882ff0f32c
Josef Ilg <jo-ilg@t-online.de>
2021-06-02 22:50:32 +0200

    4

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 this.addEventListener('fetch', event =>  {
-	event.respondWith(new Response(
+	event.respondWith(
+		return new Response(
           new Blob(
             ["<h2>I'm in your browser, ",
        	     "stealing your files</h2>\n",

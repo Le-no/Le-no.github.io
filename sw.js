18bc04fb9328cae5624f10d1d4a5951f6414bb69
Josef Ilg <jo-ilg@t-online.de>
2021-06-02 22:28:23 +0200

    4

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 this.addEventListener('fetch', event =>  {
-	  console.log('installing');
+	e.respondWith(new Response(
+          new Blob(
+            ["<h2>I'm in your browser, ",
+       	     "stealing your files</h2>\n"],
+            {type : 'text/html'})));
 });

a79081b9fdd1487b1d2bdf5d56ee448238a0d86a
Josef Ilg <jo-ilg@t-online.de>
2021-06-02 23:03:34 +0200

    4

--- sw.js
+++ sw.js
@@ -3,6 +3,6 @@ this.addEventListener('fetch', event =>  {
 		new Blob(
             ["<h2>I'm in your browser, ",
        	     "stealing your files</h2>\n",
-	    event.request.headers('User-Agent')],
+	    event.request.headers.get('User-Agent')],
             {type : 'text/html'})));
 });
